9755f281e4e99b0da54bc83ffdee9d4e276027a6
kevit-lav-ughareja <lav.ughareja@botprosolutions.com>
2025-05-12 16:07:18 +0530

    Fix:Specific table chose

--- JavaScript_Task/script.js
+++ JavaScript_Task/script.js
@@ -1,43 +1,37 @@
+const apiUrl = "https://reqres.in/api/users?page=1&&per_page=15";
 
-const apiUrl = 'https://reqres.in/api/users?page=1&&per_page=15';
+fetch(apiUrl, { headers: { "x-api-key": "reqres-free-v1 " } })
+  .then((response) => response.json())
 
+  .then((response) => {
+    const tableBody = document.getElementById("table").querySelector("tbody");
 
-fetch(apiUrl,{ 'headers': { 'x-api-key': 'reqres-free-v1 '} })
-    .then(response => response.json())
-    
-    .then(response => {
-       
-        const tableBody = document.querySelector('tbody');
-        
-       
-        response.data.forEach(user => {
-            
-            const row = document.createElement('tr');
+    response.data.forEach((user) => {
+      const row = document.createElement("tr");
 
-            const avatarCell = document.createElement('td');
-            const firstNameCell = document.createElement('td');
-            const lastNameCell = document.createElement('td');
+      const avatarCell = document.createElement("td");
+      const firstNameCell = document.createElement("td");
+      const lastNameCell = document.createElement("td");
 
-            const avatar = document.createElement('img');
-            avatar.src = user.avatar;
-            avatarCell.appendChild(avatar);
+      const avatar = document.createElement("img");
+      avatar.src = user.avatar;
+      avatarCell.appendChild(avatar);
 
-            const first_name=document.createElement('p')
-            first_name.innerText=user.first_name;
-            firstNameCell.appendChild(first_name);
+      const first_name = document.createElement("p");
+      first_name.innerText = user.first_name;
+      firstNameCell.appendChild(first_name);
 
-            const last_name=document.createElement('p')
-            last_name.innerText=user.last_name;
-            lastNameCell.appendChild(last_name);
+      const last_name = document.createElement("p");
+      last_name.innerText = user.last_name;
+      lastNameCell.appendChild(last_name);
 
-            row.appendChild(avatarCell);
-            row.appendChild(firstNameCell);
-            row.appendChild(lastNameCell);
+      row.appendChild(avatarCell);
+      row.appendChild(firstNameCell);
+      row.appendChild(lastNameCell);
 
-           
-            tableBody.appendChild(row);
-        });
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
+      tableBody.appendChild(row);
     });
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
